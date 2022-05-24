94a4417b0a3c934f001fcd724543336269a65903
Auxésio <gabrielbatista34368@gmail.com>
2022-05-24 14:37:20 -0300

    React
    revisao de como importar e exportar e um pouco de css

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,16 @@
+import Frase from './components/Frase'
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Testando CSS</h1>\
+      <Frase />
+      <Frase />
+    </div>
+  );
+}
+
+export default App;
