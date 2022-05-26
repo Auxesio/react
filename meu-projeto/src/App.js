1b9b94f635c31bae62c5f080c98488db8e3ea39b
Auxésio <gabrielbatista34368@gmail.com>
2022-05-26 17:11:09 -0300

    A

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,20 +1,23 @@
-import './App.css';
-import Evento from './components/Eventos'
-import Form from './components/Form'
-import Nome from './components/SayMyName'
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Contato from './pages/contato'
+import Empresa from './pages/empresa'
+import NavBar from './components/NavBar'
 
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>Testando Eventos</h1>
-      <Nome nome='heisenberg'/>
-      <Evento numero='1' />
-      <Evento numero='2'/>
-      <Form />
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/empresa" element={<Empresa/>} />
+        <Route path="/contato" element={<Contato/>} />
+      </Routes>
+      
+    </Router>
   );
 }
 
-export default App;
+export default App
